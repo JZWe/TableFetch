7f859a598dc0f786d01d037a192ff45ebec7b21b
ArturiaTW <ArturiaTW@gmail.com>
2023-08-08 00:18:11 +0800

    feat: 引入通用元件

---
+++ frontend/src/ui/AppLayout.tsx
@@ -0,0 +1,39 @@
+import { Outlet } from 'react-router-dom';
+import styled from 'styled-components';
+import Header from './Header';
+
+const StyledAppLayout = styled.div`
+  width: 100%;
+  height: 100vh;
+`;
+
+const Main = styled.main`
+  background-color: var(--color-grey-50);
+  padding: 4rem 4.8rem 6.4rem;
+  overflow: hidden;
+  display: flex;
+`;
+
+const Container = styled.div`
+  width: 100%;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  gap: 3.2rem;
+  overflow: hidden;
+`;
+
+function AppLayout() {
+  return (
+    <StyledAppLayout>
+      <Header />
+      <Main>
+        <Container>
+          <Outlet />
+        </Container>
+      </Main>
+    </StyledAppLayout>
+  );
+}
+
+export default AppLayout;
